e9c7ea189e827f4b1b7adddb30a3fef6feaf655e
Piotr Sakowski <p.sakowski1213@gmail.com>
2023-04-18 18:58:24 +0200

    defines Action enum & InquirerAnswers type, and init startApp

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,27 @@
 const inquirer = require('inquirer');
 
-inquirer.prompt([{
-  name: 'name',
-  type: 'input',
-  message: 'What\'s your name?',
-}, {
-  name: 'age',
-  type: 'number',
-  message: 'How old are you?',
-  default: 18,
-}]).then((answers: { name: string, age: number}) => {
-  console.log(`\nHi ${answers.name}. ${answers.age}? Nice! \n`);
-});
+enum Action {
+  List = "list",
+  Add = "add",
+  Remove = "remove",
+  Quit = "quit"
+}
+
+type InquirerAnswers = {
+  action: Action
+}
+
+const startApp = () => {
+  inquirer.prompt([{
+    name: 'action',
+    type: 'input',
+    message: 'How can I help you?',
+  }]).then((answers: InquirerAnswers) => {
+    console.log("Chosen action: " + answers.action);
+    startApp();
+    if (answers.action === "quit")
+      return;
+  });
+}
+
+startApp();
